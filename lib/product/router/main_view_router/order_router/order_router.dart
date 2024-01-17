13246675fdeabc40c69d2a734ade1c505bbb5dc0
Nuhcan <nuhcanatar20@gmail.com>
2024-01-17 15:42:21 +0300

    Update order_router.dart

--- lib/product/router/main_view_router/order_router/order_router.dart
+++ lib/product/router/main_view_router/order_router/order_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kahve/feature/main_view/home/view/productdetail/productdetail_view.dart';
 import 'package:kahve/feature/main_view/profile/view/orders/view/orderdetail/orderdetail_view.dart';
 
 class OrderServiceRouter {
@@ -9,6 +10,19 @@ class OrderServiceRouter {
       context,
       MaterialPageRoute(
         builder: (context) => OrderDetailView(
+          data: data,
+        ),
+      ),
+    );
+  }
+
+  // product detail view
+  void productDetailViewNavigatorRouter(
+      BuildContext context, Map<String, dynamic> data) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ProductDetailView(
           data: data,
         ),
       ),
