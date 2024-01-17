b2717d99274642ef21fdfd83c6bd0ca42caf019c
Nuhcan <nuhcanatar20@gmail.com>
2024-01-17 15:43:07 +0300

    Update order_db.dart

--- lib/product/utility/database/order_db/order_db.dart
+++ lib/product/utility/database/order_db/order_db.dart
@@ -3,10 +3,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 enum OrderServiceDB {
+  PRODUCTS,
   ORDERSETTING,
   ORDERPRODUCTS;
 
   // one-time query parameter
+  Future<DocumentSnapshot<Map<String, dynamic>>> productRef(
+          Map<String, dynamic> data) =>
+      FirebaseFirestore.instance.collection(name).doc(data['PRODUCTID']).get();
 
   // list query parameter
   Stream<QuerySnapshot> get orderSettingList => FirebaseFirestore.instance
