68b60d6ff453388a132410cd9bce70be0726d798
Nuhcan <nuhcanatar20@gmail.com>
2024-01-18 08:09:19 +0300

    Update order_base.dart
    Dinamik internet kontrol yapısı siparişlerim sayfasına entegre edildi

--- lib/product/utility/base/main_view_base/order_base/order_base.dart
+++ lib/product/utility/base/main_view_base/order_base/order_base.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:data_connection_checker_nulls/data_connection_checker_nulls.dart';
 import 'package:flutter/material.dart';
+import 'package:kahve/feature/main_view/noconnection/noconnection_view.dart';
 import 'package:kahve/product/router/main_view_router/order_router/order_router.dart';
+import 'package:logger/logger.dart';
 import '../../../../../product/extension/view_size.dart';
 
 abstract class MainOrderBase<T extends StatefulWidget> extends State<T> {
@@ -11,4 +16,26 @@ abstract class MainOrderBase<T extends StatefulWidget> extends State<T> {
 
   late final maxWidth = ViewSizeModelExtension(context).mediaSize.width;
   late final maxHeight = ViewSizeModelExtension(context).mediaSize.height;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      checkControl();
+    });
+  }
+
+  void checkControl() async {
+    bool result = await DataConnectionChecker().hasConnection;
+    if (result == true) {
+      Logger().i("İnternet Bağlandı!!");
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const NoConnectionErrorView(),
+        ),
+      );
+    }
+  }
 }
