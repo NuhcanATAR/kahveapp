6cb6f5ab9ffae649da74d945ffe78b3c9941efab
Nuhcan <nuhcanatar20@gmail.com>
2024-01-18 08:12:27 +0300

    Update search_base.dart
    Dinamik internet kontrol yapısı arama bölümüne entegre edildi

--- lib/product/utility/base/main_view_base/search_base/search_base.dart
+++ lib/product/utility/base/main_view_base/search_base/search_base.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:data_connection_checker_nulls/data_connection_checker_nulls.dart';
 import 'package:flutter/material.dart';
+import 'package:kahve/feature/main_view/noconnection/noconnection_view.dart';
 import 'package:kahve/product/model/main_view_model/search_model/search_model.dart';
 import 'package:kahve/product/router/main_view_router/search_router/search_router.dart';
+import 'package:logger/logger.dart';
 import '../../../../../product/extension/view_size.dart';
 
 abstract class MainSearchBase<T extends StatefulWidget> extends State<T> {
@@ -16,4 +21,26 @@ abstract class MainSearchBase<T extends StatefulWidget> extends State<T> {
 
   late final maxWidth = ViewSizeModelExtension(context).mediaSize.width;
   late final maxHeight = ViewSizeModelExtension(context).mediaSize.height;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      checkControl();
+    });
+  }
+
+  void checkControl() async {
+    bool result = await DataConnectionChecker().hasConnection;
+    if (result == true) {
+      Logger().i("İnternet Bağlandı!!");
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const NoConnectionErrorView(),
+        ),
+      );
+    }
+  }
 }
