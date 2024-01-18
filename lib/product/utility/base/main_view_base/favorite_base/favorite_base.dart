5df064d55fd13f37a80cd9da1c63107bdea7eb8b
Nuhcan <nuhcanatar20@gmail.com>
2024-01-18 08:04:53 +0300

    Update favorite_base.dart
    Favori Sayfasına Dinamik İnternet Kontrol Yapısı Entegre Edildi

--- lib/product/utility/base/main_view_base/favorite_base/favorite_base.dart
+++ lib/product/utility/base/main_view_base/favorite_base/favorite_base.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:data_connection_checker_nulls/data_connection_checker_nulls.dart';
 import 'package:flutter/material.dart';
+import 'package:kahve/feature/main_view/noconnection/noconnection_view.dart';
 import 'package:kahve/product/router/main_view_router/home_router/home_router.dart';
+import 'package:logger/logger.dart';
 import '../../../../../product/extension/view_size.dart';
 
 abstract class MainFavoriteBase<T extends StatefulWidget> extends State<T> {
@@ -12,4 +17,26 @@ abstract class MainFavoriteBase<T extends StatefulWidget> extends State<T> {
 
   late final maxWidth = ViewSizeModelExtension(context).mediaSize.width;
   late final maxHeight = ViewSizeModelExtension(context).mediaSize.height;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      checkControl();
+    });
+  }
+
+  void checkControl() async {
+    bool result = await DataConnectionChecker().hasConnection;
+    if (result == true) {
+      Logger().i("İnternet Bağlandı!!");
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const NoConnectionErrorView(),
+        ),
+      );
+    }
+  }
 }
